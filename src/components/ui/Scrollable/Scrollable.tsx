b3dfaca1d36f1f5e8dd52c1fbdd9d4f49c5d2815
InJRaven <kuuhaku989898@gmail.com>
2025-06-11 13:45:53 +0700

    Change Name Scrollable to ScrollArea and Update Logic

--- src/components/ui/Scrollable/Scrollable.tsx
+++ src/components/ui/Scrollable/Scrollable.tsx
@@ -1,53 +1,63 @@
-import { ComponentPropsWithoutRef, ComponentRef, forwardRef } from "react";
+import { ComponentPropsWithoutRef, Ref } from "react";
 import {
   Root,
+  Viewport,
   Corner,
   ScrollAreaScrollbar,
   ScrollAreaThumb,
 } from "@radix-ui/react-scroll-area";
 import { cn } from "@/lib/utils";
 
-const ScrollArea = forwardRef<
-  ComponentRef<typeof Root>,
-  ComponentPropsWithoutRef<typeof Root>
->(({ className, children, ...props }, ref) => (
-  <Root
-    ref={ref}
-    className={cn("relative overflow-hidden", className)}
-    {...props}
-  >
-    <div className="h-full w-full rounded-[inherit]">{children}</div>
-    <ScrollBar />
-    <Corner />
-  </Root>
-));
-ScrollArea.displayName = Root.displayName;
+function ScrollArea({
+  className,
+  viewportClassName,
+  children,
+  viewportRef,
+  ...props
+}: ComponentPropsWithoutRef<typeof Root> & {
+  viewportRef?: Ref<HTMLDivElement>;
+  viewportClassName?: string;
+}) {
+  return (
+    <Root
+      data-slot="scroll-area"
+      className={cn("relative overflow-hidden", className)}
+      {...props}
+    >
+      <Viewport
+        ref={viewportRef}
+        className={cn("h-full w-full rounded-[inherit]", viewportClassName)}
+      >
+        {children}
+      </Viewport>
+      <ScrollBar />
+      <Corner />
+    </Root>
+  );
+}
 
-const ScrollBar = forwardRef<
-  ComponentRef<typeof ScrollAreaScrollbar>,
-  ComponentPropsWithoutRef<typeof ScrollAreaScrollbar>
->(({ className, orientation = "vertical", ...props }, ref) => (
-  <ScrollAreaScrollbar
-    ref={ref}
-    orientation={orientation}
-    className={cn(
-      "flex touch-none select-none transition-colors",
-      orientation === "vertical" &&
-        "h-full w-2.5 border-l border-l-transparent p-[1px]",
-      orientation === "horizontal" &&
-        "h-2.5 flex-col border-t border-t-transparent p-[1px]",
-      className
-    )}
-    {...props}
-  >
-    <ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
-  </ScrollAreaScrollbar>
-));
-ScrollBar.displayName = ScrollAreaScrollbar.displayName;
+function ScrollBar({
+  className,
+  orientation = "vertical",
+  ...props
+}: ComponentPropsWithoutRef<typeof ScrollAreaScrollbar>) {
+  return (
+    <ScrollAreaScrollbar
+      data-slot="scroll-area-scrollbar"
+      orientation={orientation}
+      className={cn(
+        "flex touch-none select-none transition-colors",
+        orientation === "vertical" &&
+          "h-full w-2 border-l border-l-transparent p-[1px]",
+        orientation === "horizontal" &&
+          "h-2 flex-col border-t border-t-transparent p-[1px]",
+        className
+      )}
+      {...props}
+    >
+      <ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    </ScrollAreaScrollbar>
+  );
+}
 
-const Scrollable = {
-  Area: ScrollArea,
-  Bar: ScrollBar,
-};
-
-export { Scrollable };
+export { ScrollArea, ScrollBar };
